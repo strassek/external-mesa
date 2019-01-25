f511a89e818a97a5c025aefbaa34d07cf79f68a8
Kevin Strasser <kevin.strasser@intel.com>
2019-01-24 16:11:11 -0800

    dri: Add config attributes for color channel shift
    The existing mask attributes can only support up to 32 bpp. Introduce
    per-channel SHIFT attributes that indicate how many bits, from lsb towards
    msb, the bit field is offset. A shift of -1 will indicate that there is no
    bit field set for the channel.
    
    As old loaders will still be looking for masks, we set the masks to 0 for
    any formats wider than 32 bpp.
    
    Signed-off-by: Kevin Strasser <kevin.strasser@intel.com>
    Reviewed-by: Adam Jackson <ajax@redhat.com>

--- src/mesa/main/context.c
+++ src/mesa/main/context.c
@@ -1544,9 +1544,9 @@ check_compatible(const struct gl_context *ctx,
        ctxvis->foo != bufvis->foo)     \
       return GL_FALSE
 
-   check_component(redMask);
-   check_component(greenMask);
-   check_component(blueMask);
+   check_component(redShift);
+   check_component(greenShift);
+   check_component(blueShift);
    check_component(depthBits);
    check_component(stencilBits);
 
